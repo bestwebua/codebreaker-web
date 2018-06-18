d348022592b8ac6c7f506164bb3693254edeb366
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-06-19 00:00:04 +0300

    Update app logic

--- Gemfile
+++ Gemfile
@@ -3,3 +3,9 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 gem 'rspec'
 gem 'rack'
 gem 'codebreaker2018', '>= 0.3.1.0'
+
+
+group :development, :test do
+  gem 'pry'
+  gem 'pry-byebug'
+end
--- lib/web.rb
+++ lib/web.rb
@@ -1,5 +1,7 @@
 require 'erb'
 require 'codebreaker'
+require 'pry'
+
 
 module Codebreaker
   class Web
@@ -10,7 +12,7 @@ module Codebreaker
       new(env).response.finish
     end
 
-    attr_reader :request
+    #attr_reader :request
 
     def initialize(env)
       localization_dir = File.expand_path('./locale/.', File.dirname(__FILE__))
@@ -19,9 +21,10 @@ module Codebreaker
     end
 
     def response
-      case request.path
+      case @request.path
         when '/' then Rack::Response.new(render('index.html.erb'))
-        when '/start_game' then Rack::Response.new(request.params[:player_name])
+        when '/change_lang' then change_lang
+        #when '/start_game' then Rack::Response.new(@request.params[:player_name])
         else Rack::Response.new(render('404.html.erb'), 404)
       end
     end
@@ -29,6 +32,19 @@ module Codebreaker
     def render(template)
       path = File.expand_path("../views/#{template}", __FILE__)
       ERB.new(File.read(path)).result(binding)
+    end
+
+    def cookies
+      @request.cookies['lang']
+    end
+
+    private
+    def change_lang
+      Rack::Response.new do |response|
+        response.set_cookie('lang', @request.params['lang'])
+        response.redirect('/')
+        @locale.lang = @request.cookies['lang']
+      end
     end
   end
 end
