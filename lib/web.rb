69bbe599a9df25cb3a2a1a9600cf23cc372231e0
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-06-19 21:05:37 +0300

    Update Web#initialize

--- lib/web.rb
+++ lib/web.rb
@@ -1,9 +1,9 @@
-require 'erb'
+require_relative 'session'
 
 module Codebreaker
   class Web
     include Message
-    #include Storage
+    include Session
 
     def self.call(env)
       new(env).response.finish
@@ -14,14 +14,17 @@ module Codebreaker
     def initialize(env)
       @request = Rack::Request.new(env)
       @locale = request.session.options[:locale]
+      define_session_accessors
     end
 
     def response
       case request.path
-        when '/'
-          locale.lang = request.cookies['lang'].to_sym if request.cookies['lang']
-          Rack::Response.new(render('index.html.erb'))
-        when '/change_lang' then change_lang
+        when '/' then index
+        when '/change_lang'   then change_lang
+        when '/start_game'    then start_game
+        when '/show_hint'     then show_hint
+        when '/submit_answer' then submit_answer
+        when '/finish_game'   then finish_game
         else Rack::Response.new(render('404.html.erb'), 404)
       end
     end
@@ -32,7 +35,8 @@ module Codebreaker
     end
 
     def cookies
-      request.cookies['lang']
+      #Cookies: #{request.cookies} 
+      # "Params: #{request.session.keys}"
     end
 
     private
@@ -41,6 +45,30 @@ module Codebreaker
         response.set_cookie('lang', request.params['lang'])
         response.redirect('/')
       end
+    end
+
+    def index
+      locale.lang = request.cookies['lang'].to_sym if request.cookies['lang']
+      Rack::Response.new(render('index.html.erb'))
+    end
+
+    def start_game
+      Rack::Response.new do |response|
+        response.set_cookie('level', request.params['level'])
+        response.redirect('/')
+      end
+    end
+
+    def show_hint
+      ###
+    end
+
+    def submit_answer
+      ###
+    end
+
+    def finish_game
+      ###
     end
   end
 end
