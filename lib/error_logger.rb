bf42cf6d2d49c0042b467251d2decb3e8bd393f7
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-07-01 20:43:51 +0300

    Complete ErrorLogger middleware

--- lib/error_logger.rb
+++ lib/error_logger.rb
@@ -1,16 +1,27 @@
-require 'pry'
-
 module Codebreaker
   class ErrorLogger
+    attr_reader :request, :status
+
     def initialize(app)
       @app = app
     end
 
     def call(env)
-      Rack::Request.new(env)
-      status, headers, response = @app.call(env)
-      #status # как получить статус отданный предыдущим слоем? Его тут просто нет...
+      @status, headers, response = @app.call(env)
+      @request = Rack::Request.new(env)
+      write_log if [403, 404, 500].include?(status)
       Rack::Response.new(response, status, headers)
+    end
+
+    private
+
+    def write_log
+      log = File.expand_path('../error.log', File.dirname(__FILE__))
+      File.open(log, 'a+') { |data| data.puts error_data }
+    end
+
+    def error_data
+      "#{request.ip} #{status} #{request.path} #{Time.now}"
     end
   end
 end
