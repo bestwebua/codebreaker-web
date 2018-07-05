729c6fe6b152e185af9b91264ee600434f48eb5c
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-07-05 20:27:57 +0300

    Update rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,8 @@ lib = File.join(File.dirname(__FILE__), '../lib/*.rb')
 end
 
 RSpec.configure do |config|
+
+  config.include Rack::Test::Methods
   
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
